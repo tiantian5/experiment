6fb4c7bd470ad3cef3f513b0ae93168ca873713c
tzw <1140223352@qq.com>
2020-12-01 17:38:57 +0800

    update code

---
+++ src/main/java/com/experiment/core/service/productiveconsumption/StockCalibrator.java
@@ -0,0 +1,13 @@
+package com.experiment.core.service.productiveconsumption;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tzw
+ * @description 库存校验器
+ * @create 2020-12-01 5:21 下午
+ **/
+
+@Component
+public class StockCalibrator {
+}
